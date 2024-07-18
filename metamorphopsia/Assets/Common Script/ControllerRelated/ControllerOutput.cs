7d57c6e65f87589c88be5b656d627fa086106cd0
Saad Tahir <saadtahir28@gmail.com>
2024-07-18 12:47:23 +0500

    Initial Commit

---
+++ metamorphopsia/Assets/Common Script/ControllerRelated/ControllerOutput.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.InputSystem;
+
+public class ControllerOutput : MonoBehaviour
+{
+    static public Vector2 leftaxisDirection;
+    static public Vector2 rightaxisDirection;
+
+    static public bool pressPrimaryButton = false;
+    static public bool pressMenuButton = false;
+
+    void Start()
+    {
+        SteamVR.Initialize();
+    }
+
+    void Update()
+    {
+        leftaxisDirection = Vector2.zero;
+        rightaxisDirection = Vector2.zero;
+
+#if UNITY_EDITOR
+        if (Keyboard.current.wKey.isPressed) leftaxisDirection.y += 1;
+        if (Keyboard.current.sKey.isPressed) leftaxisDirection.y -= 1;
+        if (Keyboard.current.aKey.isPressed) leftaxisDirection.x -= 1;
+        if (Keyboard.current.dKey.isPressed) leftaxisDirection.x += 1;
+
+        if (Keyboard.current.iKey.isPressed) rightaxisDirection.y += 1;
+        if (Keyboard.current.kKey.isPressed) rightaxisDirection.y -= 1;
+        if (Keyboard.current.jKey.isPressed) rightaxisDirection.x -= 1;
+        if (Keyboard.current.lKey.isPressed) rightaxisDirection.x += 1;
+
+        pressMenuButton = Keyboard.current.backspaceKey.wasPressedThisFrame;
+        pressPrimaryButton = Keyboard.current.spaceKey.wasPressedThisFrame;
+
+#else
+        if (SteamVR_Actions.mixedreality_LeftPadTrackerPressed.state)
+            leftaxisDirection = SteamVR_Actions.mixedreality_LeftPadTracker.GetAxis(SteamVR_Input_Sources.LeftHand);
+
+        if (SteamVR_Actions.mixedreality_RightPadTrackerPressed.stateUp)
+            rightaxisDirection = SteamVR_Actions.mixedreality_RightPadTracker.GetAxis(SteamVR_Input_Sources.RightHand);
+
+        pressMenuButton = SteamVR_Actions.mixedreality_PressMenu.stateUp;
+        pressPrimaryButton = SteamVR_Actions.mixedreality_PressTrigger.stateUp;
+#endif
+    }
+}
---
+++ metamorphopsia/Assets/Scenes/Amsler Grid Test/Customed Grid/Correction/MoveVertexController.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using CustomGrid;
+
+public class MoveVertexController : MonoBehaviour
+{
+    public static UnityEvent ChangeGrid = new UnityEvent();
+    public static UnityEvent ShowDotsUI = new UnityEvent();
+    public static UnityEvent ResetSelectedPointPosition = new UnityEvent();
+
+    static private int selectedRowLine;
+    static private int selectedColumnLine;
+    static private int selectedPoint;
+
+    private bool selectionState = false;
+
+    private Vector3[] vertices;
+    static private int width, height;
+
+    public float speed;
+
+    static public bool showGrid = true;
+    MaterialPropertyBlock materialBlock;
+
+    bool Boundary(int index, Vector2 direction)
+    {
+        Vector3 xVector = Vector3.zero, yVector = Vector3.zero;
+        if (direction.x > 0 && direction.y > 0)
+        {
+            yVector = vertices[index + GridGeneration.Instance().GetWidthVerticesNumber()] - vertices[index];
+            xVector = vertices[index + 1] - vertices[index];
+        }
+        else if (direction.x > 0 && direction.y < 0)
+        {
+            yVector = vertices[index - GridGeneration.Instance().GetWidthVerticesNumber()] - vertices[index];
+            xVector = vertices[index + 1] - vertices[index];
+        }
+        else if (direction.x < 0 && direction.y > 0)
+        {
+            yVector = vertices[index + GridGeneration.Instance().GetWidthVerticesNumber()] - vertices[index];
+            xVector = vertices[index - 1] - vertices[index];
+        }
+        else if (direction.x < 0 && direction.y < 0)
+        {
+            yVector = vertices[index - GridGeneration.Instance().GetWidthVerticesNumber()] - vertices[index];
+            xVector = vertices[index - 1] - vertices[index];
+        }
+
+        if (Vector3.Dot(xVector.normalized, yVector.normalized) <= -0.98)
+            return false;
+
+        return true;
+    }
+
+    bool MovePoint(float speed)
+    {
+        Vector2 direction = ControllerOutput.leftaxisDirection;
+        int index = selectedPoint;
+        if (direction != Vector2.zero && Boundary(index, direction))
+        {
+            vertices[index].x += speed * direction.x;
+            vertices[index].y += speed * direction.y;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    void ChoosePoint()
+    {
+        Vector2 direction = ControllerOutput.rightaxisDirection;
+
+        float threshold = 0.6f;
+
+        if (direction == Vector2.zero)
+        {
+            selectionState = false;
+        }
+        if (direction.x >= threshold && !selectionState)
+        {
+            selectedColumnLine = (selectedColumnLine >= width - 2 ? selectedColumnLine : selectedColumnLine + 1);
+            selectionState = true;
+        }
+        else if (direction.x <= -threshold && !selectionState)
+        {
+            selectedColumnLine = (selectedColumnLine <= 1 ? selectedColumnLine : selectedColumnLine - 1);
+            selectionState = true;
+        }
+        else if (direction.y >= threshold && !selectionState)
+        {
+            selectedRowLine = (selectedRowLine >= height - 2 ? selectedRowLine : selectedRowLine + 1);
+            selectionState = true;
+        }
+        else if (direction.y <= -threshold && !selectionState)
+        {
+            selectedRowLine = (selectedRowLine <= 1 ? selectedRowLine : selectedRowLine - 1);
+            selectionState = true;
+        }
+
+        selectedPoint = selectedColumnLine + width * selectedRowLine;
+    }
+
+    void ShowDots()
+    {
+        if (showGrid)
+        {
+            for (int i = 0; i < GameObject.Find("Points").transform.childCount; ++i)
+            {
+                GameObject.Find("Points").transform.GetChild(i).gameObject.SetActive(true);
+                GameObject.Find("Grid").GetComponent<Renderer>().material.SetFloat("_ShowGrid", 1.0f);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < GameObject.Find("Points").transform.childCount; ++i)
+            {
+                GameObject.Find("Points").transform.GetChild(i).gameObject.SetActive(false);
+                GameObject.Find("Grid").GetComponent<Renderer>().material.SetFloat("_ShowGrid", 0.0f);
+            }
+        }
+    }
+
+    private void SetLineColor()
+    {
+        Vector4 lineColor = new Vector4(0, 0, 0, 1);
+        
+        GetComponent<Renderer>().material.SetInt("XSelected", selectedColumnLine);
+        GetComponent<Renderer>().material.SetInt("YSelected", selectedRowLine);
+        GetComponent<Renderer>().material.SetVector("restColor", lineColor);
+
+        if (!showGrid)
+            GameObject.Find("Points").transform.GetChild(selectedPoint).gameObject.SetActive(true);
+    }
+
+    void RefreshDotAtOneDotMode()
+    {
+        if (!showGrid)
+            GameObject.Find("Points").transform.GetChild(selectedPoint).gameObject.SetActive(false);
+    }
+
+    static public void Initilize()
+    {
+        width = GridGeneration.Instance().GetWidthVerticesNumber();
+        height = GridGeneration.Instance().GetHeightVerticesNumber();
+
+        selectedRowLine = height / 2; // 1;
+        selectedColumnLine = width / 2; // 1;
+        selectedPoint = selectedColumnLine + width * selectedRowLine;
+    }
+
+    private void Awake()
+    {
+        speed = 0.8f;
+
+        ChangeGrid.AddListener(ChangeStructure);
+        ShowDotsUI.AddListener(ShowDots);
+        ResetSelectedPointPosition.AddListener(ResetState);
+
+        materialBlock = new MaterialPropertyBlock();
+    }
+
+    private void OnDisable()
+    {
+        materialBlock.Clear();
+    }
+
+    private void Start()
+    {
+        Initilize();
+    }
+
+    private void ResetState()
+    {
+        selectedRowLine *= 2;
+        selectedColumnLine *= 2;
+
+        speed *= 0.8f;
+    }
+
+    private void ChangeStructure()
+    {
+        float moveSpeed = Time.deltaTime * speed;
+
+        width = GridGeneration.Instance().GetWidthVerticesNumber();
+        height = GridGeneration.Instance().GetHeightVerticesNumber();
+
+        vertices = GetComponent<MeshFilter>().mesh.vertices;
+
+        RefreshDotAtOneDotMode();
+        ChoosePoint();
+        if (MovePoint(moveSpeed))
+        {
+            GridDecoration.changed = true;
+            GetComponent<MeshFilter>().mesh.SetVertices(vertices);
+        }
+
+        SetLineColor();
+    }
+}
