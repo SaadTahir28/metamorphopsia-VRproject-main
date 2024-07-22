116089c3505a2475779e20ce10133e6567d18a57
SaadTahir <saadtahir28@gmail.com>
2024-07-22 17:38:04 +0500

    Added external camera for correction

---
+++ metamorphopsia/Assets/Saad/Webcam.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Webcam : MonoBehaviour
+{
+    public Material material;
+    private WebCamTexture webcamTexture;
+
+    private void OnEnable()
+    {
+        // Check for available webcams
+        if (WebCamTexture.devices.Length > 0)
+        {
+            Debug.Log("Camera Found");
+            // Use the first available webcam
+            WebCamDevice device = WebCamTexture.devices[0];
+            webcamTexture = new WebCamTexture(device.name);
+            webcamTexture.Play();
+
+            // Assign the webcam texture to both materials
+            material.mainTexture = webcamTexture;
+        }
+        else
+        {
+            Debug.LogError("No webcam found");
+            enabled = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Clear the textures when no longer active
+        if (material != null) material.mainTexture = null;
+
+        // Stop the webcam
+        if (webcamTexture != null)
+        {
+            Debug.Log("Stopping Camera");
+            webcamTexture.Stop();
+        }
+    }
+}
